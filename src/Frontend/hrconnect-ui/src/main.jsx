5d9624fc67213b3afdb5e33276ea9adcb52ecdd8
Sharath Devaraj <Sharathndevaraj8@gmail.com>
2026-08-15 22:53:39 +0530

    Google Client ID

--- src/Frontend/hrconnect-ui/src/main.jsx
+++ src/Frontend/hrconnect-ui/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <GoogleOAuthProvider
+            clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+        >
+            <App />
+        </GoogleOAuthProvider>
+    </React.StrictMode>
+);
